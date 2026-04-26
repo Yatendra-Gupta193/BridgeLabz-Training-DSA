734faa0c6e2f26895078516e29ae5874c8e00589
Yatendra Kumar Gupta <guptajikrish12@gmail.com>
2026-04-27 01:23:25 +0530

    ReverseLinkedList.java

---
+++ Linked-List/ReverseLinkedList.java
@@ -0,0 +1,55 @@
+public class ReverseLinkedList {
+
+    // Node class
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+  
+    // Function to reverse linked list
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;    // store next node
+            current.next = prev;    // reverse link
+            prev = current;         // move prev
+            current = next;         // move current
+        }
+
+        return prev; // new head
+    }
+
+    // Print linked list
+    public static void printList(Node head) {
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Creating list: 1->2->3->4->5
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Original Linked List:");
+        printList(head);
+
+        head = reverse(head);
+
+        System.out.println("\nReversed Linked List:");
+        printList(head);
+    }
+}
